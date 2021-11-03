d9d3cfb50d977da7b367a80aae3b0b89406586af
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-11-03 18:16:38 +0200

    Date and time fixed

--- client/src/Components/Display/Plane.jsx
+++ client/src/Components/Display/Plane.jsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 
 function Plane(props) {
-    const dateOptions = { timeZone: 'UTC', month: 'long', day: 'numeric', year: 'numeric' };
-
-const dateFormatter = new Intl.DateTimeFormat('en-US', dateOptions);
-const dateAsFormattedString = dateFormatter.format(new Date(props.data.arrival_time));
-
+    var myDate = props.data.arrival_time;
+    var date = new Date(myDate).toISOString().substring(0, 10);
+    console.log(date)
+    const time = new Date(myDate).toLocaleTimeString('en',
+                 { timeStyle: 'short', hour12: false, timeZone: 'UTC' });
+                 console.log(time);
     return (
         <div className="card m-1" style={{width: '18rem'}}>
             <div className="card-body">
                 <h5 className="card-title">From: {props.data.from_town}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Arrival time: {dateAsFormattedString}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Arrival Date: {date}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Arrival time: {time}</h6>
                 <h6 className="card-subtitle mb-2 text-muted">Is it late: {props.data.is_late === 1 ? "Yes" : "No"}</h6>
                 <p className="card-text">Airline: {props.data.airline}</p>
                 <button type="button" className="btn btn-light m-3" onClick={() => props.delete(props.data.id)}>DELETE SCOOTER</button>
--- client/src/Components/Table/PlaneForTable.jsx
+++ client/src/Components/Table/PlaneForTable.jsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 
 function PlaneForTable(props) {
-
+    var myDate = props.data.arrival_time;
+    var date = new Date(myDate).toISOString().substring(0, 10);
+    console.log(date)
+    const time = new Date(myDate).toLocaleTimeString('en',
+                 { timeStyle: 'short', hour12: false, timeZone: 'UTC' });
+                 console.log(time);
     return (
             <tr>
                 <td>{props.data.id}</td>
                 <td>{props.data.from_town}</td>
-                <td>{props.data.arrival_time}</td>
+                <td>{date} {time}</td>
                 <td>{props.data.is_late === 1 ? "Yes" : "No"}</td>
                 <td>{props.data.airline}</td>
                 <td>
